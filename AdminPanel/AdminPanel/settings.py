8d783ee23bcff1fa399e70f50a2114dded3114c3
anna-levina <anna_levina@internet.ru>
2024-03-12 16:35:30 +0300

    Добавлен ALLOWED_HOSTS

--- AdminPanel/AdminPanel/settings.py
+++ AdminPanel/AdminPanel/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-h#-6g+!a$9mhcozq1m5euh(e#w^ij^j!j+dxz&x1wyq05d5jmk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.183.128']
 API_DOMAIN = api_domain
 
 # Application definition
