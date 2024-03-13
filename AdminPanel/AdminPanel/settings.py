28d753ae0a8919c0e7cd13208c9f514ac9830bad
levina-anna <anna_levina@internet.ru>
2024-03-13 16:15:49 +0300

    Добавлен STATICFILES_STORAGE

--- AdminPanel/AdminPanel/settings.py
+++ AdminPanel/AdminPanel/settings.py
@@ -109,6 +109,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'DataTableApp/static/DataTableApp')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
