3dac3dea167eaf2e2f661b8475ec3af8dc3fe15e
levina-anna <anna_levina@internet.ru>
2024-04-28 15:11:35 +0300

    Улучшена обработка исключений. Определен URL API на уровне модуля.

--- AdminPanel/DataTableApp/modules/cost/main.py
+++ AdminPanel/DataTableApp/modules/cost/main.py
@@ -2,33 +2,34 @@ import requests
 from config import api_domain
 
 
-def get_cost() -> list:
-    # URL API, с которого мы хотим получать данные
-    api_url = f"{api_domain}/products_and_categories"
+# URL API, с которого мы хотим получать данные
+api_url = f"{api_domain}/products_and_categories"
 
+
+def get_cost() -> list:
     # Отправляем GET-запрос к API
     response = requests.get(api_url)
-
-    # Проверяем, что запрос прошёл успешно
-    if response.status_code == 200:
-        # Преобразуем ответ API в JSON
-        data = response.json()
-        # Возвращаем данные
-        return data
-    else:
-        # Обрабатываем возможные ошибки
-        print(f"Error getting data from API: {response.status_code}")
+    try:
+        # Проверяем, что запрос прошёл успешно
+        if response.status_code == 200:
+            # Преобразуем ответ API в JSON
+            data = response.json()
+            # Возвращаем данные
+            return data
+        else:
+            # Обрабатываем возможные ошибки
+            print(f"Error getting data from API: {response.status_code}")
+            return []
+    except requests.RequestException as e:
+        # Логируем исключение, если что-то пошло не так
+        print(f"An error occurred while connecting to the API: {e}")
         return []
 
 
 def get_categories() -> list:
-    # URL API, с которого получаем данные
-    api_url = f"{api_domain}/products_and_categories"
-
+    # Отправляем GET-запрос к API
+    response = requests.get(api_url)
     try:
-        # Отправляем GET-запрос к API
-        response = requests.get(api_url)
-
         # Проверяем, что запрос прошёл успешно
         if response.status_code == 200:
             # Преобразуем ответ API в JSON
