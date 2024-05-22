679211d01419e8a5d65a82b8f43d97bcf16cf057
levina-anna <anna_levina@internet.ru>
2024-05-22 23:32:38 +0300

    Update main.py

--- AdminPanel/DataTableApp/modules/cost/main.py
+++ AdminPanel/DataTableApp/modules/cost/main.py
@@ -2,49 +2,49 @@ import requests
 
 
 def get_cost(api_url) -> list:
-    # Отправляем GET-запрос к API
+    # Send a GET request to the API
     response = requests.get(api_url)
     try:
-        # Проверяем, что запрос прошёл успешно
+        # Check that the request was successful
         if response.status_code == 200:
-            # Преобразуем ответ API в JSON
+            # Convert the API response to JSON
             data = response.json()
-            # Возвращаем данные
+            # Return data
             return data
         else:
-            # Обрабатываем возможные ошибки
+            # Handle possible errors
             print(f"Error getting data from API: {response.status_code}")
             return []
     except requests.RequestException as e:
-        # Логируем исключение, если не удалось подключиться к API
+        # Log an exception if it was not possible to connect to the API
         print(f"An error occurred while connecting to the API: {e}")
         return []
 
 
 def get_categories(api_url) -> list:
-    # Отправляем GET-запрос к API
+    # Send a GET request to the API
     response = requests.get(api_url)
     try:
-        # Проверяем, что запрос прошёл успешно
+        # Check that the request was successful
         if response.status_code == 200:
-            # Преобразуем ответ API в JSON
+            # Convert the API response to JSON
             data = response.json()
 
-            # Создаем словарь для отслеживания уникальных категорий
+            # Create a dictionary to track unique categories
             categories = {}
             for item in data:
-                # Добавляем категорию в словарь, если ее там еще нет
+                # Add a category to the dictionary if it is not there yet
                 cat_id = item['category_id']
                 categories[cat_id] = item['category_name']
 
-            # Возвращаем список уникальных категорий
+            # Return a list of unique categories
             unique_categories = [{'category_id': k, 'category_name': v} for k, v in categories.items()]
             return unique_categories
         else:
-            # В случае ошибки возвращаем пустой список
+            # In case of error, return an empty list
             print(f"Error getting data from API: {response.status_code}")
             return []
     except requests.RequestException as e:
-        # Логируем исключение, если не удалось подключиться к API
+        # Log an exception if it was not possible to connect to the API
         print(f"An error occurred while connecting to the API: {e}")
         return []
